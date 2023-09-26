bf74335fca02c4c94643171958528a93002b649a
DolphieDude <andreycherevat@gmail.com>
2023-09-27 00:13:11 +0200

    initial commit. Web page with possibility to import excel files to DB

---
+++ DBManager.js
@@ -0,0 +1,107 @@
+const mysql   = require('mysql');
+const express = require("express");
+const app = express();
+
+
+const urlencodedParser = express.urlencoded({extended: false});
+// встановлює Handlebars як двигун представлень в Express
+app.set("view engine", "hbs");
+
+
+// зміна, в якій зберігаються дані для підключення до БД
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'qwerty12',
+    database : 'ecoMon'
+});
+
+
+// підключення до БД
+connection.connect(function(err){
+    if (err) {
+        return console.error("Error-connect: " + err.message);
+    }
+    else{
+        console.log("Connection to MySQL OK!");
+    }
+});
+
+
+
+
+// звертаємося до корню програми і отримуємо дані з БД та виводимо їх на екран у вигляді таблиці
+app.get("/", function(req, res){
+    connection.query("SELECT * FROM object", function(err, data) {
+        if(err) return console.log(err);
+        res.render("index.hbs", {
+            object: data
+            // object - назва вашої таблиці
+        });
+    });
+});
+
+
+
+
+// запит get до серверу верне нам форму для додання нового об'єкта
+app.get("/add", function(req, res){
+    res.render("add.hbs");
+});
+// Після заповнення форми та натискання на кнопку дані в запиті POST відправляються методом, що отримує відправлені дані і за допомогою SQL-команди INSERT відправляє їх в БД
+app.post("/add", urlencodedParser, function (req, res) {
+
+    if(!req.body) return res.sendStatus(400);
+    const name = req.body.name;
+    const description = req.body.description;
+    connection.query("INSERT INTO object(name, description) VALUES (?,?)", [name, description], function(err, data) {
+        if(err) return console.log(err);
+        res.redirect("/");
+    });
+});
+
+
+
+
+// При натисканні на редагування у списку об'єктів наступному методу в GET-запиті передається id об'єкта. Метод отримує id і ним витягує з БД необхідний об'єкт і передається його форму у поданні edit.hbs.
+app.get("/edit/:idobject", function(req, res){
+    const idobject = req.params.idobject;
+    connection.query("SELECT * FROM object WHERE idobject=?", [idobject], function(err, data) {
+        if(err) return console.log(err);
+        res.render("edit.hbs", {
+            object: data[0]
+// object - назва вашої таблиці
+        });
+    });
+});
+// Після редагування та натискання на кнопку дані надсилаються у POST-запиті. Метод отримує дані та за допомогою команди UPDATE відправляє їх у БД.
+app.post("/edit", urlencodedParser, function (req, res) {
+
+    if(!req.body) return res.sendStatus(400);
+    const name = req.body.name;
+    const description = req.body.description;
+    const idobject = req.body.idobject;
+
+    connection.query("UPDATE object SET name=?, description=? WHERE idobject=?", [name, description, idobject], function(err, data) {
+        if(err) return console.log(err);
+        res.redirect("/");
+    });
+});
+
+
+
+
+// При натисканні на кнопку видалення у списку об'єктів спрацьовує метод, який отримує id об'єкта, що видаляється, і видаляє його з БД за допомогою команди DELETE.
+app.post("/delete/:idobject", function(req, res){
+
+    const idobject = req.params.idobject;
+    connection.query("DELETE FROM object WHERE idobject=?", [idobject], function(err, data) {
+        if(err) return console.log(err);
+        res.redirect("/");
+    });
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+});
---
+++ import-excel.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const bodyparser = require('body-parser');
+const readXlsxFile = require('read-excel-file/node');
+const mysql = require('mysql2');
+const multer = require('multer');
+const app = express();
+
+app.use(express.static('./public'));
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
+
+var connection = mysql.createConnection({
+    host    :'localhost',
+    user    :'root',
+    password:'7720',
+    database:'shevchuk'
+});
+
+connection.connect(function(err){
+    if (err){
+        return console.error("Error-connect: ", err.message);
+    }
+    else {
+        console.log("Connection to MyFuckingSql is Fine!");
+    }
+});
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, process.cwd() + '/uploads/')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now() + '-' + file.originalname)
+    },
+})
+const uploadFile = multer({storage: storage})
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/import-object', uploadFile.single('import-object'),
+    (req, res) => {
+        importFileToDb(process.cwd() + '/uploads/' + req.file.filename, 'object(name, description, ownership)');
+        console.log(res);
+    })
+
+app.post('/import-pollutant', uploadFile.single('import-pollutant'),
+    (req, res) => {
+        importFileToDb(process.cwd() + '/uploads/' + req.file.filename, 'pollutant(name_pollutant, mass_consumption)');
+        console.log(res);
+    })
+
+app.post('/import-pollution', uploadFile.single('import-pollution'),
+    (req, res) => {
+        importFileToDb(process.cwd() + '/uploads/' + req.file.filename, 'pollution(idobject, idpollutant, valuepollution, year)');
+        console.log(res);
+    })
+
+function importFileToDb(exFile, query){
+    readXlsxFile(exFile).then((rows) => {
+        rows.shift();
+        connection.query('insert into ' + query + ' values ?', [rows], (error, response) => {
+            console.log(error || response);
+        })
+    })
+}
+
+const port = 3000;
+app.listen(port, () => {
+    console.log('App listnening on port ' + port);
+});
